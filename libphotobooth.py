c7a3f73ca0f42302a14d50d2666e7b0e7f954693
root <icenyne@gmail.com>
2015-05-13 11:47:19 -0500

    first commit of photobooth script...

---
+++ libphotobooth.py
@@ -0,0 +1,290 @@
+#!/usr/bin/python
+import threading
+import time
+import piggyphoto
+import os, sys
+import shutil
+import pygame
+from serial import Serial
+from string import split,join
+from pygame.locals import *
+from templates import templates
+from random import randrange
+import Image, select, v4l2capture
+from StringIO import StringIO
+
+# set up serial port for button box and lens light
+# and turn on all three buttons...
+buttonpattern = [0, 1, 2, 4, 2, 1, 0, 4, 2, 1, 0, 1, 2, 4, 0, 2, 5, 2, 5, 7, 0, 7, 0, 7]
+ser = Serial(port = '/dev/ttyACM0')
+ser.write(str(7))
+
+# list with raw image suffixes, used for appending to files as they are created
+suffix = [ 'a', 'b', 'c', 'd' ]
+
+# execute a shell command, printing it to the console for debugging purposes...
+def shellcmd(command):
+	print ' =>', command
+	os.system(command)
+
+# new filename generation function: returns next sequential filename
+def new_filename(storename='lastphoto', increment=True):
+	last = eval(open(storename, 'r').read())
+	filename = 'DSC' + (4-len(str(last)))*'0' + str(last)
+	if increment: last=last+1
+	open(storename, 'w').write(str(last))
+	return filename
+
+
+# generate a composite image from a template, horizontal or vertical, abstracted 
+#    and genericized so that various combinations of templates can be created...
+def generate_composite(template, filename, blocking=True, generateprint=False):
+	# base graphicsmagick commands to generate composites...
+#	interrim = 'gm composite -resize &ARG1 -geometry &ARG2 &FILENAME_&I.jpg &ARG3 -quality 98 &ARG4 '
+	interrim = templates[template][-2]
+	ending = templates[template][-1]
+	
+	# generate interrim commands...
+	for i in range(1,5):
+		command = join(split(interrim, '&FILENAME'), filename)
+		command = join(split(command, '&I'), suffix[i-1])
+		for j in range(4):
+			command = join(split(command, '&ARG'+str(j+1)), templates[template][i-1][j])
+		if blocking:
+			while (filename+'_'+suffix[i-1]+'_done' not in os.listdir(os.curdir)):
+				pass #if block is true, wait until the file is there 
+		shellcmd(command)
+
+	# generate ending command...
+	command = join(split(ending, '&FILENAME'), filename)
+	command = join(split(command, '&TEMPLATE'), template)
+	for j in range(4):
+		command = join(split(command, '&ARG'+str(j+1)), templates[template][4][j])
+	shellcmd(command)
+
+	# copy the phone version to the print version so the printing computer will see it...
+	if template == 'phone-2x3':
+		print 'template', template
+		shellcmd('cp '+filename+'_'+template+'.jpg '+filename+'_print-2x3.jpg')
+
+	# do this extra step to make a double print strip, 
+	#    assumes use of 'phone' template/ending file...
+	#    (requires a vertical 2000x6000 final composite image to start)
+	if generateprint and template=='phone-2x3': 
+		print 'template:', template
+		shellcmd('gm composite -geometry +0+0 ' + filename +'_'+ template + '.jpg images/background-big.jpg -quality 100 done.jpg')
+		shellcmd('gm composite -geometry +2001+0 ' + filename +'_'+ template +'.jpg done.jpg -quality 100 done.jpg')
+		template = template[5:]
+		shellcmd('gm convert -stroke gray -draw "line 2000,0 2000,6000" done.jpg -quality 95 ' + filename + '_print'+template+'.jpg')
+	if generateprint and template=='phone-1x3': 
+		print 'template:', template
+		shellcmd('gm composite -geometry +0+0 ' + filename +'_'+ template + '.jpg images/background-big2k3k.jpg -quality 100 done.jpg')
+		shellcmd('gm composite -geometry +1001+0 ' + filename +'_'+ template +'.jpg done.jpg -quality 100 done.jpg')
+		template = template[5:]
+		shellcmd('gm convert -stroke gray -draw "line 1000,0 1000,3000" done.jpg -quality 95 ' + filename + '_print'+template+'.jpg')
+
+# generate print fascade to generate_composite...
+def generate_print(template, filename, blocking=True, generateprint=True):
+	generate_composite(template, filename, blocking, generateprint)
+
+# delete the temporary files created during creation of composites...
+def cleanup_temp_files(filename):
+	# remove temp files...
+	shellcmd('rm *output.jpg done.jpg '+filename+'*done')
+
+# filename function: returns next sequential filename (as a string)
+def new_filename(storename='lastphoto', increment=True):
+	last = eval(open(storename, 'r').read())
+	filename = 'DSC' + (4-len(str(last)))*'0' + str(last)
+	if increment: last=last+1
+	open(storename, 'w').write(str(last))
+	return filename
+
+
+def startvid():			# open the video camera for use...
+	global x, y, video
+        video = v4l2capture.Video_device('/dev/video0')
+        x, y = video.set_format(1280, 720, fourcc='MJPG')
+	video.create_buffers(1)
+        video.queue_all_buffers()
+	video.start()
+
+# function to grab the sequence of raw images. 
+#    copied dummy images to a new 'filename' for testing 
+#    so that camera does not need to be used...
+def grab_image(filename, i, usecamera=True):
+	# Only capture image if it's one of the four... 
+	if i in range(4): 
+		# grab from camera or make a copy of the dummy images (for testing...)
+		buff = StringIO()	# create StringIO buffer for stashing image data
+		for jj in range(3 ):
+			select.select((video,), (), ())
+			image_data = video.read_and_queue()	# grab image from camera
+			buff.write(image_data)			# stash in buff
+			buff.seek(0)				# reset file pointer to zero...
+			im = Image.open(buff)			# read image from buff
+			buff.seek(0)
+		ix, iy = im.size
+		im.crop( (100, 0, ix-100, iy) ).resize( (1620, 1080), Image.ANTIALIAS ).save(filename+'_'+suffix[i]+'.jpg')
+
+	# create flag file indicating that photo file download from camera completed...
+	#shellcmd('gm convert -shave 100x0 '+filename+'_'+suffix[i]+'.jpg '+filename+'_'+suffix[i]+'.jpg')
+	open(filename+'_'+suffix[i]+'_done', 'w').write('done') 
+
+
+# move files into local subdirectories and SAMBA share at path
+def move_files(filename, path='/media/PHOTOBOOTH/', copy=True):
+      if copy: cmd='cp '
+      else: cmd='mv '
+      try:
+	print
+	print 'filename = ', filename
+	print cmd+'raw images...'
+	shellcmd(cmd+filename+'_[a-d].jpg '+path+'raw-images')
+	print cmd+'phone image...'
+        shellcmd(cmd+filename+'_phone*.jpg '+path+'for-phone')
+	print cmd+'print images...'
+        shellcmd(cmd+filename+'_print*.jpg '+path+'for-print')
+	print cmd+'display image...'
+        shellcmd(cmd+filename+'_display*.jpg '+path+'for-display')
+	print cmd+'lastone.txt...'
+	shellcmd(cmd+'lastone.txt '+path)
+      except:
+	print 'PROBLEMS!!'
+
+
+#size = width, height = 960, 540
+#camerasize = camw, camh =  810,540
+size = width, height = 1920, 1080
+#size = width, height = 1280, 1024
+scrsize = scrw, scrh =  1920, 1080
+scrloc = (width-scrw)/2, (height-scrh)/2
+#camerasize = camw, camh =  1280, 720
+#camerasize = camw, camh =  1080, 720
+camerasize = camw, camh =  1620, 1080
+cameraloc = (width-camw)/2, (height-camh)/2
+#dispsize = disw, dish = 1056, 720
+dispsize = disw, dish = 1584, 1080
+disploc = (width-disw)/2, (height-dish)/2
+
+black = (0,0,0)
+white = (255,255,255)
+
+def lightsoff():
+	ser.write(str(0)) #turn off all lights...
+
+def blinklenslight():
+	for i in range(5):
+		ser.write(str(8)) # flash lens ring lighjt
+		time.sleep(0.04)
+		ser.write(str(0))
+		time.sleep(0.04)
+	ser.write(str(8))	# leave lens ring light on when done...
+
+def waitforkey(key, quitable = True, timeout = 99999999):
+	# check for button lighting...
+	if len(key)==1: # looking for a specific key, so light just that one...
+		ser.write(str(0))
+		time.sleep(0.1)
+		if key[0]==K_g: ser.write(str(4))
+		if key[0]==K_y: ser.write(str(2))
+		if key[0]==K_r: ser.write(str(1))
+		blink = False
+	else: 
+		blink = True
+		blinky = 0
+
+	userkey = False
+	while not(userkey):
+	   if timeout > 0:
+		timeout -= 1
+		time.sleep(0.2)
+		if blink:
+			ser.write(str(buttonpattern[blinky]))
+			if blinky<len(buttonpattern)-1: blinky += 1
+			else: blinky = 0
+		for event in pygame.event.get():
+			#print repr(event)
+			if event.type == QUIT: sys.exit()
+			elif event.type == KEYDOWN: 
+				#print 'keydown...'
+				if event.key in key:
+					ser.write(str(0)) # turn off button lights... 
+					return event.key
+				if quitable and event.key == K_q: sys.exit()
+	   else:
+		# if we end up here, we timed out. Return "t" key...
+		return K_t
+	pygame.event.clear()
+
+
+def fillscreen(screen, color):
+	screen.fill(color)
+	pygame.display.flip()
+
+def displayimage(screen, filename, csize, location=(0,0)):
+		image = pygame.image.load(filename)
+		imagerect = image.get_rect()
+		image = pygame.transform.scale(image, csize)
+		screen.blit(image, location)
+		pygame.display.flip()
+
+def flashtext(duration, rate, screen, text, size, location=None):
+	bgwhite = pygame.Surface(screen.get_size())
+	bgblack = pygame.Surface(screen.get_size())
+	bgwhite = bgwhite.convert()
+	bgblack = bgblack.convert()
+	bgwhite.fill(white)
+	bgblack.fill(black)
+	
+	fontname = pygame.font.match_font('freeserif')
+	font = pygame.font.Font(fontname, 128)
+	textw = font.render(text, 1, white)
+	textb = font.render(text, 1, black)
+	textwpos = textw.get_rect()
+	textbpos = textb.get_rect()
+	if location==None:
+		textwpos.centerx = textbpos.centerx = bgwhite.get_rect().centerx	
+		textwpos.centery = textbpos.centery = bgwhite.get_rect().centery
+	else:
+		w,h = location
+		textwpos.centerx = textbpos.centerx = w
+		textwpos.centery = textbpos.centery = h
+	bgwhite.blit(textb, textbpos)
+	bgblack.blit(textw, textbpos)
+
+	start = time.time()
+	while (time.time()-start < duration):
+		screen.blit(bgblack, (0,0))
+		pygame.display.flip()
+		time.sleep(rate/2.)
+		screen.blit(bgwhite, (0,0))
+		pygame.display.flip()
+		time.sleep(rate/2.)
+
+
+def showtext(screen, text, size, location=None):
+	bgwhite = pygame.Surface(screen.get_size())
+	bgwhite = bgwhite.convert()
+	bgwhite.fill(black)#white)
+	
+	fontname = pygame.font.match_font('freeserif')
+	font = pygame.font.Font(fontname, size)
+	textb = font.render(text, 1, white)#black)
+
+	textbpos = textb.get_rect()
+	if location==None:
+		textbpos.centerx = bgwhite.get_rect().centerx	
+		textbpos.centery = bgwhite.get_rect().centery
+	else:
+		w,h = location
+		textbpos.centerx = w	
+		textbpos.centery = h
+	bgwhite.blit(textb, textbpos)
+
+	screen.blit(bgwhite, (0,0))
+	pygame.display.flip()
+
+
+
+
---
+++ photobooth.py
@@ -0,0 +1,259 @@
+#!/usr/bin/python
+# many dependencies are all brought in with this import...
+from libphotobooth import *
+
+#=============================================================================
+# ========================= COMMAND LINE ARGUMENTS ===========================
+#=============================================================================
+if 'help' in sys.argv or '-h' in sys.argv or '--help' in sys.argv:
+	print """
+NAME: 
+	photobooth.py  -  a photo booth python script
+
+OPTIONS:
+	-h,--help,help	print help
+	nousecamera	use dummy photos instead of camera (default: USB connected camera)
+	nomove		do not move images after processing (default: move)
+	lastphoto=xxxx	begin sequence with the provided 4-digit (>=1000) number 
+	noincrement	do not increment the image sequence number (default: increment)
+	doubleprint	generate the double print (adds time, default: no doubleprint)
+	sepia		do composites in sepia tone
+	bw		do composites in black and white
+	1x3		generate phone at 1000x3000 reslution (faster) for double prints
+	2x6		generate phone and print in 2000x3000 resolution (faster)
+	4x6		generate phone and print in 4000x6000 resolution (too slow)
+	regen=DSCxxx	filename to regenerate
+	location=<path>	path to raw images with _a, _b, _c, _d suffixes of filename
+	nodisplay	do not use the graphical display or delays with them (default = true)
+	recurse		used with regen to generate composites from all photos in raw directory
+
+DESCRIPTION:
+	This python script implements a photo booth where a sequence of four images is 
+	taken by a USB-connected camera and processed into various composite images.
+
+	Requires: libgphoto2, python-pygame, piggyphoto (python binding for libgphoto2)
+	and graphicsmagick.
+
+"""
+	sys.exit()
+
+# use camera or dummy source images...
+if 'nousecamera' in sys.argv:
+	camera_arg=False
+else:
+	camera_arg=True
+
+if 'doubleprint' in sys.argv:
+	doubleprint=True
+else:
+	doubleprint=False
+
+if 'nodisplay' in sys.argv:
+	display = False
+else:
+	display = True
+
+# use sepia tone...
+default_tone = '1x3'
+if 'sepia' in sys.argv and not('bw' in sys.argv):
+	default_tone = '-sepia'
+if 'bw' in sys.argv and not('sepia' in sys.argv):
+	default_tone='-bw'
+if '4x6' in sys.argv and not('sepia' in sys.argv) and not('bw' in sys.argv):
+	default_tone='-4x6'
+if '2x3' in sys.argv and not('sepia' in sys.argv) and not('bw' in sys.argv):
+	default_tone='-2x3'
+if '1x3' in sys.argv and not('sepia' in sys.argv) and not('bw' in sys.argv):
+	default_tone='-1x3'
+
+# move the files when done? Assume true...
+move=True
+if 'nomove' in sys.argv:
+	print 'Not moving files...'
+	move=False
+
+# set lastphoto via command line... 
+lastphoto=False
+for i in sys.argv:
+	if 'lastphoto' in i:
+		lastphoto = True
+		temp = split(i, '=')[1]
+		break
+if not(lastphoto):
+	# this should be rolled into the filename function but for now it's here...
+	last = eval(open('lastphoto', 'r').read())
+	print 'Change current photo number '+str(last)+'?'
+	temp = raw_input( 'Enter valid new number or nothing to keep: ')
+if temp not in ['']: 
+	last = eval(temp) 
+	open('lastphoto', 'w').write(str(last))
+
+# increment output photo index? default is true...
+increment=True
+if 'noincrement' in sys.argv:
+	increment = False
+
+# for regenerating a photo, need to specify base filename and location of raws...
+regenerate = False
+for i in sys.argv:
+	if 'regen' in i:
+		regenerate = True
+	if 'location' in i:
+		location = split(i, '=')[1]
+
+# regenerate all files from raws...
+recurse = False
+loop = xrange(10000) # effectively while(1)...
+if 'recurse' in sys.argv and regenerate:
+	recurse = True
+	rawfiles = os.listdir(location+'raw-images/')
+	loop = []
+	for i in rawfiles:
+		if i[0:7] not in loop: loop.append(i[0:7])
+	loop.sort()
+
+#=============================================================================
+# ===================== DONE COMMAND LINE ARGUMENTS ==========================
+#=============================================================================
+
+
+
+#=============================================================================
+# ==================================  MAIN  ==================================
+#=============================================================================
+
+# verify command line args...
+print 'nousecamera:', repr(camera_arg)
+print 'nomove:', repr(not(move))
+print 'lastphoto:', last
+print 'increment:', repr(increment)
+print 'doubleprint:', repr(doubleprint)
+print 'tone:', repr(default_tone)
+
+if display:
+	pygame.init()
+	screen = pygame.display.set_mode(size)
+
+startvid()
+
+for element in loop:
+	# flush the key queue in the event that someone hit it...
+	# important when looping, not so much the first time into the loop...
+	if display: pygame.event.clear()
+
+	if display:
+		fillscreen(screen, black)
+		if default_tone in ['-1x3', '-2x3', '-4x6']:
+			displayimage(screen, 'images/pushtostart-nochoice.jpg', scrsize, scrloc)
+		else:	
+			displayimage(screen, 'images/pushtostart.jpg', scrsize, scrloc)
+
+		key = waitforkey([K_g, K_r, K_y])	
+	if default_tone=='':
+		if key == K_y: tone='-sepia'
+		if key == K_r: tone='-bw'
+		if key == K_g: tone =''
+	else:
+		tone=default_tone
+
+	if display:
+		fillscreen(screen, black)
+		displayimage(screen, 'images/fourphotostaken.jpg', scrsize, scrloc)
+		time.sleep(3.5)
+		fillscreen(screen, black)
+		displayimage(screen, 'images/pushbuttontocontinue.jpg', scrsize, scrloc)
+		keylist = [K_r, K_g, K_y]
+		key = waitforkey(keylist, timeout=13)
+		if key == K_t: continue	
+		fillscreen(screen, black)
+
+	# keep track of the starting time for some statistics...
+	start = time.time()
+	
+	# get a new filename and print it to the console...
+	if not(recurse):
+		filename= new_filename(increment=increment)
+	else: 
+		filename = element # this comes from the raw directory...
+	# filename is pre-filled above from command line... 
+	print '\r\nnew filename:', filename
+
+	# prime threads for compositing images...
+	t_ = []
+	t_.append( threading.Thread(target=generate_composite, args=('display'+tone, filename)) )
+	if not(doubleprint): t_.append( threading.Thread(target=generate_composite, args=('phone'+tone, filename)) )
+	else: t_.append( threading.Thread(target=generate_print, args=('phone'+tone, filename)) )
+	# start the queued threads...
+	for i in t_: i.start()
+
+	# grab the sequence of images from the camera (or, if specified, dummy images)...
+	for i in range(4):
+		if display:
+			#showtext(screen, 'Image: '+str(i+1), 100)
+			displayimage(screen, 'images/image'+str(i+1)+'.jpg', scrsize, scrloc)
+			#time.sleep(0.75)
+			blinklenslight()
+		print 
+		print 'Grabbing image: ', i+1
+		if display: fillscreen(screen, black)
+		if not(regenerate): 
+			# get image from camera...
+			grab_image(filename, i, camera_arg)
+		else:
+			# if regenerate, copy from location the appropriate raw image...
+			# and create semaphore file to indicate completion...
+			print 'Copying:', location + filename+'_'+suffix[i] + '.jpg'
+			shellcmd('cp ' + location + 'raw-images/' + filename+'_'+suffix[i] + '.jpg' + ' .')
+			open(filename+'_'+suffix[i]+'_done', 'w').write('done') 
+		lightsoff() # turn off lens ring light...
+		if display: displayimage(screen, filename+'_'+suffix[i]+'.jpg', camerasize, cameraloc)
+		print 'time to display:', time.time()-start
+		if display: time.sleep(3)
+
+	# wait until all compositing threads are complete...
+	living=True
+	displayed=False
+	while ( living ):# or t_print.isAlive() ): 
+		living=False
+		if not displayed: 
+		    	if display:	fillscreen(screen, black)
+			time.sleep(0.5)
+			if display: showtext(screen, 'Processing...', 100)
+			time.sleep(0.5)
+		else: time.sleep(1)
+		print '    ===> still processing...'	
+		for i in t_: 
+			if i.isAlive(): living=True
+		if not(t_[0].isAlive()) and not(displayed):
+			displayed=True
+			print 'time to display:', time.time()-start
+			if display: 
+				displayimage(screen, filename+'_display'+tone+'.jpg', dispsize, disploc)
+				disptime = time.time()
+
+	if time.time() - disptime < 10:
+		time.sleep(10 - (time.time() - disptime) ) 
+
+	print '\r\nAll images done:', time.time()-start
+	time.sleep(1)
+
+	# clean up the temporary files generated during compositing...
+	cleanup_temp_files(filename)
+	open('lastone.txt', 'w').write(filename)
+
+	# move files (default) to redundant locations...
+	if move and not(regenerate):
+		move_files(filename, path='/var/www/html/', copy=False)
+
+	if move and regenerate:
+		move_files(filename, path=location, copy=False)
+
+	# print elapsed time to console...
+	print '\r\nDone: ', time.time()-start
+
+
+	# only do loop once if we're regenerating a set of composites...
+	if regenerate and not recurse: break
+
+
+
