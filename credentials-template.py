24e899f2f5958280708eb7c2bd2dd70ea12491d1
root <ftfpbk@users.noreply.github.com>
2015-05-21 13:36:36 -0500

    added credentials.py template, to aid in moving to another board...

---
+++ credentials-template.py
@@ -0,0 +1,14 @@
+# Replace the strings with your particular info 
+# and then rename this file to credentials.py
+#
+ftphost = 'ftphostname.com' 
+ftpname = 'ftploginname'
+ftppass = 'ftppassword'
+smtphost = 'smtpemailhostname.com'
+smtpname = 'smtpusername'
+smtppass = 'smtppassword'
+
+# this string needs the trailing "/"
+# it should point fully to the directory on the web
+# server where the images are being uploaded via ftp.
+webhost = 'http://webhost.where.com/theimage/goes/'
